102819045c52154bf9013e2c6ac2d506b8c54e30
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2024-10-23 16:33:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "rahull"
-vpc_security_groups_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-05f044979e305302e"]
 tags = {
 Name = var.instance_names[count.index]
 }
