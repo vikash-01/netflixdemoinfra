3207fc391ad1cdcfce2c8345bcff56ff79590e8f
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2025-03-25 19:42:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "argocd"
+vpc_security_group_ids = ["sg-06785bc60f40dceda"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "Monitoring server"]
+default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
