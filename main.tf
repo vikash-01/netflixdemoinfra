166a3f2c3492c33994b8e04b50300ee29a0e2cc4
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2025-09-03 16:22:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-04aa00acb1165b32a"
-instance_type = "t2.medium"
-key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
-tags = {
-Name = var.instance_names[count.index]
-}
+  count                  = 4
+  ami                    = "ami-0360c520857e3138f"
+  instance_type          = "t2.medium"
+  key_name               = "terraformkeypair"
+  vpc_security_group_ids = ["sg-0f1d5f2f18bb4af01"]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
 }
 
 variable "instance_names" {
-default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
