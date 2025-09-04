2b91f90c63225ad0cbcb484995f90b7107362537
vikash-01 <95972203+vikash-01@users.noreply.github.com>
2025-09-04 11:22:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0360c520857e3138f"
   instance_type          = "t2.medium"
-  key_name               = "terraformkeypair"
-  vpc_security_group_ids = ["sg-0f1d5f2f18bb4af01"]
+  key_name               = "Aws"
+  vpc_security_group_ids = ["sg-07ccb3cd9c9e3fa65"]
   tags = {
     Name = var.instance_names[count.index]
   }
